4f9e6b5706d889745a2de0a3e0a697694ac6e4d1
prudhvi <prudhvi.php1@gmail.com>
2023-05-17 17:12:28 +0530

    OP1-02: Injecting the custom service into Controller.

--- modules/custom/firstmodule/src/Controller/HelloDrupalController.php
+++ modules/custom/firstmodule/src/Controller/HelloDrupalController.php
@@ -3,10 +3,38 @@
 namespace Drupal\firstmodule\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\firstmodule\HelloWorldSalutation;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 
 class HelloDrupalController extends ControllerBase {
 
+  /**
+   * The salutation service.
+   * Adding the custom Salutation service here
+   *
+   * @var \Drupal\firstmodule\FirstModuleSalutation
+   */
+  protected $salutation;
+
+  /**
+   * HelloDrupalController constructor.
+   *
+   * @param \Drupal\firstmodule\FirstModuleSalutation $salutation
+   *   The salutation service.
+   */
+  public function __construct(FirstModuleSalutation $salutation) {
+    $this->salutation = $salutation;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('firstmodule.salutation')
+    );
+  }
   /**
    * Defining the Controller method for the '/hello' path custom route
    */
