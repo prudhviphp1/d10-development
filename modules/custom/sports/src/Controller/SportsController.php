ba5ced83f3e7de9548c0fcb4777709fe9f67d123
prudhvi <prudhvi.php1@gmail.com>
2024-04-25 08:23:13 +0530

    OP1-22a: Running phpcs & phpcbf checks.

--- modules/custom/sports/src/Controller/SportsController.php
+++ modules/custom/sports/src/Controller/SportsController.php
@@ -9,66 +9,70 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 /**
  * Controller for the Sports module.
  */
-class SportsController extends ControllerBase {
+class SportsController extends ControllerBase
+{
 
-  /**
-   * The database connection.
-   *
-   * @var \Drupal\Core\Database\Connection
-   */
-  protected $database;
+    /**
+     * The database connection.
+     *
+     * @var \Drupal\Core\Database\Connection
+     */
+    protected $database;
 
-  /**
-   * SportsController constructor.
-   *
-   * @param \Drupal\Core\Database\Connection $database
-   *   The database connection.
-   */
-  public function __construct(Connection $database) {
-    $this->database = $database;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container) {
-    return new static(
-      $container->get('database')
-    );
-  }
-
-  /**
-   * Renders a table of players.
-   */
-  public function players() {
-    // The number of items per page.
-    $limit = 5;
-    $query = $this->database->select('players', 'p')
-      ->fields('p')
-      ->extend('\Drupal\Core\Database\Query\PagerSelectExtender')
-      ->limit($limit);
-    $result = $query->execute()->fetchAll();
-
-    $header = [$this->t('Name')];
-    $rows = [];
-
-    foreach ($result as $row) {
-      $rows[] = [
-        $row->name,
-      ];
+    /**
+     * SportsController constructor.
+     *
+     * @param \Drupal\Core\Database\Connection $database
+     *   The database connection.
+     */
+    public function __construct(Connection $database)
+    {
+        $this->database = $database;
     }
 
-    $build = [];
-    $build[] = [
-      '#theme' => 'table',
-      '#header' => $header,
-      '#rows' => $rows,
-    ];
+    /**
+     * {@inheritdoc}
+     */
+    public static function create(ContainerInterface $container)
+    {
+        return new static(
+            $container->get('database')
+        );
+    }
 
-    $build[] = [
-      '#type' => 'pager',
-    ];
+    /**
+     * Renders a table of players.
+     */
+    public function players()
+    {
+        // The number of items per page.
+        $limit = 5;
+        $query = $this->database->select('players', 'p')
+            ->fields('p')
+            ->extend('\Drupal\Core\Database\Query\PagerSelectExtender')
+            ->limit($limit);
+        $result = $query->execute()->fetchAll();
 
-    return $build;
-  }
+        $header = [$this->t('Name')];
+        $rows = [];
+
+        foreach ($result as $row) {
+            $rows[] = [
+            $row->name,
+            ];
+        }
+
+        $build = [];
+        $build[] = [
+        '#theme' => 'table',
+        '#header' => $header,
+        '#rows' => $rows,
+        ];
+
+        $build[] = [
+        '#type' => 'pager',
+        ];
+
+        return $build;
+    }
 }
